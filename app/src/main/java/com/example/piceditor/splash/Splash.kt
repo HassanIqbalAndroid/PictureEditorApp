7e5cdcbf5c873c80842e17911494fb953ecf08b4
HassaniqbalAndroid <deeplearning.developer@gmail.com>
2023-01-19 03:18:51 -0800

    UI and Pic Editor module completed

---
+++ app/src/main/java/com/example/piceditor/splash/Splash.kt
@@ -0,0 +1,25 @@
+package com.example.piceditor.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.piceditor.MainActivity
+import com.example.piceditor.databinding.ActivitySplashBinding
+
+class Splash : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            this.finish()
+
+        },3000)
+    }
+}
